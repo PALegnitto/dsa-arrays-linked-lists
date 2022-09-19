581dc8d54435a3e2cb545aa4fa565e8e5e9fd603
Phill Legnitto <palegnitto@gmail.com>
2022-09-19 16:04:22 -0700

    completed

--- linked-list.js
+++ linked-list.js
@@ -72,6 +72,7 @@ class LinkedList {
       this.length --;
       return current.val;
     } else if (current === null) {
+      throw new Error('cannot pop from empty list');
       return undefined;
     }
 
@@ -92,11 +93,34 @@ class LinkedList {
 
   shift() {
 
+    if (this.length === 0) {
+      throw new Error('cannot shift from empty list');
+      return null;
+    }
+    if (this.length === 1) this.tail = null;
+    let firstVal = this.head;
+    this.head = firstVal.next;
+    firstVal.next = null;
+    this.length--;
+
+    return firstVal.val
+
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    if (idx >= this.length || idx < 0) {
+      throw new Error('item at index does not exist');
+      return undefined;
+    }
+
+    let current = this.head;
+
+    for (let i = 0; i <= idx; i++){
+        if ( i === idx) return current.val;
+        current = current.next;
+    }
 
   }
 
@@ -104,17 +128,96 @@ class LinkedList {
 
   setAt(idx, val) {
 
+    if (idx >= this.length || idx < 0) {
+      throw new Error('item at index does not exist');
+      return undefined;
+    }
+
+    let current = this.head;
+
+    for (let i = 0; i <= idx; i++){
+        if ( i === idx) current.val = val;
+        current = current.next;
+    }
+
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
+  //
+  //
 
   insertAt(idx, val) {
 
+    if (idx > this.length || idx < 0) {
+      throw new Error('item at index does not exist');
+      return undefined;
+    }
+
+    if ( this.length === 0){
+      this.head = new Node(val);
+      this.tail = this.head;
+      this.length++;
+      return undefined;
+    }
+
+    if (idx === this.length){
+      this.push(val);
+      return undefined;
+    }
+
+    if ( idx === 0){
+      let temp = this.head;
+      this.head = new Node(val);
+      this.head.next = temp;
+      this.length++;
+      return undefined;
+    }
+
+
+    let current = this.head;
+
+    for (let i = 0; i <= idx; i++){
+        if ( (i + 1) === idx) {
+          let temp = current.next;
+          current.next = new Node(val);
+          current.next.next = temp;
+          this.length++;
+        }
+        current = current.next;
+    }
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+
+    if (idx >= this.length || idx < 0) {
+      throw new Error('item at index does not exist');
+      return undefined;
+    }
+
+    let current = this.head;
+
+    if (idx === this.length){
+      return this.pop();
+    }
+
+    if (idx === 0){
+      return this.shift();
+
+    }
+
+    for (let i = 0; i <= idx; i++){
+        if ( (i + 1) === idx) {
+
+        let temp = current.next.val;
+        current.next = current.next.next;
+        this.length--;
+
+        return temp;
+      }
+      current = current.next;
+    }
 
   }
 
@@ -122,6 +225,16 @@ class LinkedList {
 
   average() {
 
+    let sum = 0;
+    let current = this.head;
+
+    if (this.length === 0 ) return 0;
+
+    while (current !== null){
+      sum += current.val;
+      current = current.next;
+    }
+  return sum / this.length;
   }
 }
 
